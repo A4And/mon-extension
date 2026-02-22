ca04582d40563fd479acdf8c6b431085ee16a5c9
A4And <bernot.and@gmail.com>
2026-02-22 16:03:27 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    A4_Lampadaire.allumageProgressif(A4_Lampadaire.CouleurAllumage.Bleu, 100, 10)
+})
+input.onButtonPressed(Button.B, function () {
+    A4_Lampadaire.eteindreLampadaire()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+basic.forever(function () {
+	
+})
